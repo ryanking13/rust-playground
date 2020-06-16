e0c1969ceb724604a03334d61b287cdd0f66d36a
ryanking13 <def6488@gmail.com>
2020-06-16 17:53:54 +0900

    Add help/banner string

--- markdown/src/main.rs
+++ markdown/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+fn parse_markdown_file() {
+
 }
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    let mut written_by = String::from("Written by: ");
+    written_by.push_str(env!("CARGO_PKG_AUTHORS"));
+    let mut homepage = String::from("Homepage: ");
+    homepage.push_str(env!("CARGO_PKG_HOMEPAGE"));
+    let mut usage = String::from("Usage: tinymd <somefile>.md");
+    println!("{}", written_by);
+    println!("{}", homepage);
+    println!("{}", usage);
+}
+
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
+}
+
+fn usage() {
+    print_long_banner();
+}
+
+fn main() {
+    usage();
+}
+
+
